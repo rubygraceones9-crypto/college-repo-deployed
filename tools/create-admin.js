9e470d2eb6c936ed924538265dc3cf027a36f44b
Ruby Grace Oñes <ruby.ones@jmc.edu.ph>
2026-04-02 04:59:24 +0800

    Commit this changes

--- tools/create-admin.js
+++ tools/create-admin.js
@@ -16,7 +16,7 @@ require('dotenv').config({ path: path.join(__dirname, '..', '.env.local') });
 const config = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'cite_es',
 };
 
